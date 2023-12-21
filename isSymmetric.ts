542911244fb83cc4f28c3ad66f5dc1c76b9451d8
Simba <idrlzt@outlook.com>
2023-12-21 16:33:55 +0800

    update: isSymmetric function

--- isSymmetric.ts
+++ isSymmetric.ts
@@ -2,17 +2,16 @@
 import { ITreeNode } from './common/TreeNode';
 
 // DFS
-function check(p: ITreeNode | null, q: ITreeNode | null): boolean {
-	if (p === null && q === null) {
-		return true
-	} else if (p === null || q === null) {
-		return false
-	} else {
-		return p.val === q.val && check(p.left, q.right) && check(p.right, q.left)
-	}
-}
-
 function isSymmetric(root: ITreeNode | null): boolean {
+	const check = (p: ITreeNode | null, q: ITreeNode | null) => {
+		if (p === null && q === null) {
+			return true
+		} else if (p === null || q === null) {
+			return false
+		} else {
+			return p.val === q.val && check(p.left, q.right) && check(p.right, q.left)
+		}
+	}
 	return check(root.left, root.right)
 };
 
