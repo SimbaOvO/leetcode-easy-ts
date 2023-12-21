991faf2ed2cedd07ee61ed601211dfdfa1701f54
Simba <idrlzt@outlook.com>
2023-12-21 16:12:26 +0800

    feat: sortedArrayToBST_solution

---
+++ sortedArrayToBST.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+import { ITreeNode, TreeNode } from './common/TreeNode';
+
+function sortedArrayToBST(nums: number[]): ITreeNode | null {
+	const toBST = (left: number, right: number) => {
+		if (left > right) {
+			return null
+		}
+		
+		const mid = Math.floor((left + right) / 2)
+		let currentBST = new TreeNode(nums[mid]);
+		
+		currentBST.left = toBST(left, mid - 1)
+		currentBST.right = toBST(mid + 1, right)
+		
+		return currentBST
+	}
+	
+	return toBST(0, nums.length - 1)
+};
