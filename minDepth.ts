bb10988d61922e1ed84014bb9988ea65b2f3f048
Simba <idrlzt@outlook.com>
2023-12-22 17:44:16 +0800

    update: add minDepth BFS solution

--- minDepth.ts
+++ minDepth.ts
@@ -21,3 +21,32 @@ function minDepth(root: ITreeNode | null): number {
 	
 	return Math.min(leftDepth, rightDepth) + 1
 };
+
+// BFS
+function minDepth(root: ITreeNode | null): number {
+	if (root === null) {
+		return 0
+	}
+	
+	if (root.left === null && root.right === null) {
+		return 1
+	}
+	
+	const nodes = [root]
+	let depth = 0
+	while(nodes.length > 0) {
+		depth++
+		
+		let size = nodes.length
+		while(size > 0) {
+			const node = nodes.shift()
+			if (node.left === null && node.right === null) {
+				return depth
+			}
+			
+			node.left && nodes.push(node.left)
+			node.right && nodes.push(node.right)
+			size--
+		}
+	}
+};
