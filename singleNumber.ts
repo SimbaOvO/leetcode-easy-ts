d27c22b3fa13702be404a68c43b3c91504fee3fd
Simba <idrlzt@outlook.com>
2023-12-26 20:37:21 +0800

    update: singleNumber map to set

--- singleNumber.ts
+++ singleNumber.ts
@@ -1,13 +1,13 @@
 // https://leetcode.com/problems/single-number/
 
 function singleNumber(nums: number[]): number {
-	const map = new Map()
+	const set = new Set()
 	
 	for (let i = 0; i < nums.length; i++) {
-		if (!map.delete(nums[i])) {
-			map.set(nums[i], nums[i])
+		if (!set.delete(nums[i])) {
+			set.add(nums[i])
 		}
 	}
 	
-	return map.values().next().value
+	return set.values().next().value
 };
