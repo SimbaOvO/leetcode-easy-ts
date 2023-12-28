e11dd5a454f041b13da6bd69b0e2f22c8c3c2013
Simba <idrlzt@outlook.com>
2023-12-28 16:47:47 +0300

    update: postorderTraversal new solution

--- binary-tree/postorderTraversal.ts
+++ binary-tree/postorderTraversal.ts
@@ -12,3 +12,33 @@ function postorderTraversal(root: ITreeNode | null): number[] {
 	
 	return [...left, ...right, root.val]
 };
+
+// iterative
+function postorderTraversal(root: TreeNode | null): number[] {
+	if (root === null) {
+		return []
+	}
+	
+	let node = root
+	const stack = []
+	const arr = []
+	
+	while (root || stack.length > 0) {
+		while (root) {
+			stack.push(root)
+			root = root.left
+		}
+		
+		const last = stack.pop()
+		
+		if (last.right === null || last.right === node) {
+			arr.push(last.val)
+			node = last
+		} else {
+			stack.push(last)
+			root = last.right
+		}
+	}
+	
+	return arr
+};
