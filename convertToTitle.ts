04c520202f55aec4a1590b3114d48a875a4c78b4
Simba <idrlzt@outlook.com>
2023-12-31 20:21:47 +0800

    update: excel-column-number solution

--- convertToTitle.ts
+++ convertToTitle.ts
@@ -2,12 +2,12 @@
 
 function convertToTitle(columnNumber: number): string {
 	const mod = 26;
-	let str = "";
+	let str = '';
 	
 	// ...x3 * 26^2 + x2 * 26^1 + x1 * 26^0 = columnNumber
 	while (columnNumber > 0) {
 		const charIndex = (columnNumber - 1) % mod;
-		str = String.fromCharCode("A".charCodeAt(0) + charIndex) + str;
+		str = String.fromCharCode('A'.charCodeAt(0) + charIndex) + str;
 		columnNumber = Math.floor((columnNumber - 1) / mod);
 	}
 	
--- titleToNumber.ts
+++ titleToNumber.ts
@@ -2,12 +2,11 @@
 
 function titleToNumber(columnTitle: string): number {
 	const base = 26
-	const startCharCode = 65
+	const startCharCode = 'A'.charCodeAt(0)
 	
 	let [start, end] = [0, columnTitle.length - 1]
 	let sum = 0
 	
-	// ...x3 * 26^2 + x2 * 26^1 + x1 * 26^0 = columnNumber
 	while(end >= 0) {
 		const currentCode = columnTitle[end].charCodeAt(0) + 1 - startCharCode
 		sum += currentCode * Math.pow(base, start)
