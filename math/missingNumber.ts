5fae67e61a4fb9308b7f057ba611e22f6f6cc315
Simba <idrlzt@outlook.com>
2024-01-16 16:48:59 +0800

    update: missingNumber_solution

--- math/missingNumber.ts
+++ math/missingNumber.ts
@@ -2,9 +2,12 @@
 
 function missingNumber(nums: number[]): number {
 	const len = nums.length
-	// Arithmetic sequence
-	const total = Math.floor((len * (len + 1)) / 2)
+	// 等差数列的和
+	// 首项与末项的和，乘以项数除以2 => (1 + len) * len / 2
+	// Math.floor 兼容nums = 0的情况
+	const total = Math.floor((1 + len) * len / 2)
 	const sum = nums.reduce((prev, cur) => prev + cur, 0)
 	
+	// 做差
 	return total - sum
 };
