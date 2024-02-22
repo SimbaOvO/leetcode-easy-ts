82ef06b50395f06e27cddc19191093eb67028afa
Simba <idrlzt@outlook.com>
2024-02-22 15:39:14 +0800

    feat: 292. Nim Game_solution

---
+++ math/canWinNim.ts
@@ -0,0 +1,18 @@
+function canWinNim(n: number): boolean {
+	return n % 4 !== 0;
+	
+	// TLE @_@
+	// const dp = new Array(n + 1).fill(false)
+	// dp[1] = true
+	// dp[2] = true
+	// dp[3] = true
+	
+	// let index = 4
+	
+	// while (index <= n) {
+	//   dp[index] = !dp[index - 1] || !dp[index - 2] || !dp[index - 3]
+	//   index++
+	// }
+	
+	// return dp[n]
+};
